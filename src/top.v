b415778f690efc835950c5f63fc699f5da352eac
derzhavin3016 <derzhavin.andrej@yandex.ru>
2023-04-23 23:45:21 +0300

    Make pc public

--- src/top.v
+++ src/top.v
@@ -2,7 +2,8 @@ module top (input clk, reset,
                 output [31:0] writedata, dataadr,
                 output memwrite
                );
-    wire [31:0] pc, instr, readdata;
+    logic [31:0] pc /* verilator public */;
+    logic [31:0] instr, readdata;
     // instantiate processor and memories
     riscv riscv (clk, reset, pc, instr, memwrite, dataadr, writedata, readdata);
     imem imem (pc[7:2], instr);
