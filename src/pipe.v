49cc3d6fe4d4636b2cd4edbdd8a9a6f60320d09e
derzhavin3016 <derzhavin.andrej@yandex.ru>
2023-05-01 17:12:21 +0300

    Add pipe module

---
+++ src/pipe.v
@@ -0,0 +1,18 @@
+module rPipe #(parameter WIDTH = 32) (
+        input clk,
+        input en,
+        input clr,
+        input [(WIDTH - 1):0] inpData,
+        output [(WIDTH - 1):0] outData
+    );
+    reg [(WIDTH - 1):0] savedData;
+    assign outData = savedData;
+
+    always @(posedge clk) begin
+        if (clr)
+            savedData <= 0;
+        else if (en)
+            savedData <= inpData;
+    end
+
+endmodule
